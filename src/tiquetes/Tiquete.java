d8f8b49cf19c435d7aa2da5aade5edbaad08040f
juan-rios <juanesrios13@gmail.com>
2020-03-03 13:03:00 -0500

    tiquetes ADD

---
+++ src/tiquetes/Tiquete.java
@@ -0,0 +1,30 @@
+package tiquetes;
+
+import java.util.Date;
+
+public class Tiquete {
+	
+	protected Date fechaCompra;
+	protected String asiento;
+	protected int grupoAbordaje;
+	protected String codigoReserva;
+	protected Date horaEnSala;
+	
+	//private Vuelo vuelo;
+	//private Persona persona;
+	
+	public Tiquete(Date fechaCompra, String asiento, int grupoAbordaje, String codigoReserva, Date horaEnSala/*,Vuelo vuelo, Persona persona*/) {
+		super();
+		this.fechaCompra = fechaCompra;
+		this.asiento = asiento;
+		this.grupoAbordaje = grupoAbordaje;
+		this.codigoReserva = codigoReserva;
+		this.horaEnSala = horaEnSala;
+		/*this.vuelo = vuelo;
+		this.persona = persona;*/
+	}
+	
+	
+	
+	
+}
