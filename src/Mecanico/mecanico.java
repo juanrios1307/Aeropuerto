c6c50f3df5872a147c4f163a083fff3bd7945e47
Decol <Decol@DESKTOP-3S4OU78>
2020-03-06 20:14:54 -0500

    update

--- src/Mecanico/mecanico.java
+++ src/Mecanico/mecanico.java
@@ -1,11 +1,45 @@
 package Mecanico;
 	
-	import Persona.Persona;
+	import java.time.Period;
+import java.util.Date;
+
+import Persona.Persona;
 	
 public class mecanico {
 	
 	Persona persona;
 	private int licenciaMecanico;
+	
+	
+	public boolean aceite(boolean aceite) {
+		return aceite;
+	}
+	
+	public double pesoCombustible(double consumo, Date horaSalida, Date horaLlegada) {
+		Period p = new Period(horaSalida, horaLlegada);
+		long hours= p.getHours();
+		return 0;
+	}
+	
+	public boolean pesoBalance(boolean pesoBalance) {
+		return pesoBalance;
+	}
+	public boolean brujula(boolean brujula) {
+		return brujula;
+	}
+	public boolean altimetro(boolean altimetro) {
+		return altimetro;
+	}
+	public boolean extintores(boolean extintores) {
+		return extintores;
+	}
+	public boolean equipoOxigeno(boolean equipoOxigeno) {
+		return equipoOxigeno;
+	}
+	public boolean equipoEmergencia(boolean equipoEmergencia) {
+		return equipoEmergencia;
+	}
+	
 	
 	
 	
