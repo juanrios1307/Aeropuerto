436957f0d8ae4d3c3325a8341fda7505a74e7785
Decol <Decol@DESKTOP-3S4OU78>
2020-03-08 18:06:51 -0500

    migracioon???????

---
+++ src/Aeropuerto/Migracion.java
@@ -0,0 +1,15 @@
+package Aeropuerto;
+
+public class Migracion {
+	
+	public Migracion(Vuelo[] vueloLlegada, Vuelo[] vueloSalida) {
+		super();
+		this.vueloLlegada = vueloLlegada;
+		this.vueloSalida = vueloSalida;
+	}
+
+	Vuelo[] vueloLlegada;
+	Vuelo[] vueloSalida;
+	
+	
+}
