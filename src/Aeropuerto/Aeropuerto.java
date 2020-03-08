15382c3dfd04271263c17979d340af5aeaca7216
Decol <Decol@DESKTOP-3S4OU78>
2020-03-08 14:54:14 -0500

    vuelos aeropuerto como arrays

--- src/Aeropuerto/Aeropuerto.java
+++ src/Aeropuerto/Aeropuerto.java
@@ -5,23 +5,32 @@ import 	Mecanico.Mecanico;
 
 public class Aeropuerto {
 	
-	public Aeropuerto(Vuelo vueloLlegada, Vuelo vueloSalida) {
+	public Aeropuerto(Vuelo[] vueloLlegada, Vuelo[] vueloSalida) {
 		super();
 		this.vueloLlegada = vueloLlegada;
 		this.vueloSalida = vueloSalida;
 	}
 
-	Vuelo vueloLlegada, vueloSalida;
+	Vuelo[] vueloLlegada;
+	Vuelo[] vueloSalida;
+	int codigoEstadia;
 	
-	public double impuestosAeropuertuarios(Date tiempoAeropuerto) {
-		long diferencia = vueloLlegada.getHoraLlegada().getTime() - vueloSalida.getHoraSalida().getTime();
+	public double impuestosAeropuertuarios() {
+		long diferencia = vueloLlegada[getCodigoEstadia()].getHoraLlegada().getTime() - vueloSalida[getCodigoEstadia()].getHoraSalida().getTime();
 		long hours = diferencia / (60 * 60 * 1000) % 24;
 		return hours*2000000;
 	}
 	
 	public double cobroGasolina() {
-		Mecanico mecanico = new Mecanico(vueloSalida);
+		Mecanico mecanico = new Mecanico(vueloSalida[getCodigoEstadia()]);
 		return mecanico.pesoCombustible()*1620/0.804;
+	}
+
+	public int getCodigoEstadia() {
+		return codigoEstadia;
+	}
+	public void setCodigoEstadia(int codigoEstadia) {
+		this.codigoEstadia = codigoEstadia;
 	}
 	
 }
