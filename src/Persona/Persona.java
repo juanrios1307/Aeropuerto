ca0021f627775bcd756f02f9a0e5c1a05f908cb2
Silvia-Suarez <61568481+Silvia-Suarez@users.noreply.github.com>
2020-03-07 21:50:13 -0500

    Update Persona.java

--- src/Persona/Persona.java
+++ src/Persona/Persona.java
@@ -1,11 +1,14 @@
 package Persona;
-import java.util.*;
 import java.io.*;
 public class Persona implements Serializable {
 		protected String nombre,documento,nacionalidad;
 		protected int edad;
-		protected boolean coronavirus;
+		protected boolean enfermo;
 		
+		
+		public boolean isEnfermo() {
+			return enfermo;
+		}
 		public String getNombre() {
 			return nombre;
 		}
@@ -19,11 +22,13 @@ public class Persona implements Serializable {
 			return edad;
 		}
 		
-		public Persona(String nombre, String documento, String nacionalidad, int edad) {
+		public Persona(String nombre, String documento, String nacionalidad, int edad, boolean enfermo) {
+			super();
 			this.nombre = nombre;
 			this.documento = documento;
 			this.nacionalidad = nacionalidad;
 			this.edad = edad;
+			this.enfermo = enfermo;
 		}
 		
 }
