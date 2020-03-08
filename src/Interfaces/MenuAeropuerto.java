ee1d1752a61abcf3b9496a059f5255ea272e7b6e
juanse <juanse41601@gmail.com>
2020-03-07 21:05:08 -0500

    Avion,vuelo,interfaces,errores

---
+++ src/Interfaces/MenuAeropuerto.java
@@ -0,0 +1,72 @@
+package Interfaces;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class MenuAeropuerto extends JFrame{
+
+	public MenuAeropuerto() {
+		this.setSize(500, 400);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setTitle("Sistema Aeroportuario");
+		this.setLocationRelativeTo(null);
+		this.getContentPane().setBackground(Color.gray);
+		
+		iniciarComponentes();
+	}
+	
+	public void iniciarComponentes() {
+		JPanel panel=new JPanel();
+		panel.setLayout(null);
+		this.getContentPane().add(panel);
+		
+		JLabel etiqueta=new JLabel();
+		etiqueta.setText("Aeropuerto");
+		etiqueta.setBounds(85, 10, 300, 50);
+		etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
+		etiqueta.setFont(new Font("arial",Font.ITALIC,20));
+		panel.add(etiqueta);
+		
+		JButton torreControl=new JButton();
+		torreControl.setText("Torre de control");
+		torreControl.setBounds(110, 70, 250, 30);
+		torreControl.setHorizontalAlignment(SwingConstants.CENTER);
+		torreControl.setFont(new Font("arial",Font.ITALIC,20));
+		panel.add(torreControl);
+		torreControl.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				InterfazTorreControl i1=new InterfazTorreControl();
+				i1.setVisible(true);
+				setVisible(false);
+			}
+			
+		});
+		
+		JButton migracion=new JButton();
+		migracion.setText("Migracion");
+		migracion.setBounds(110, 120, 250, 30);
+		migracion.setHorizontalAlignment(SwingConstants.CENTER);
+		migracion.setFont(new Font("arial",Font.ITALIC,20));
+		panel.add(migracion);
+		migracion.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				InterfazMigracion i1=new InterfazMigracion();
+				i1.setVisible(true);
+				setVisible(false);
+			}
+			
+		});
+
+	}
+}
