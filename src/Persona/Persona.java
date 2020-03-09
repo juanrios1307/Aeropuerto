db9b2030c54c94ed29ad034765d858b4568c46ec
Silvia-Suarez <61568481+Silvia-Suarez@users.noreply.github.com>
2020-03-08 22:00:15 -0500

    Update Persona.java

--- src/Persona/Persona.java
+++ src/Persona/Persona.java
@@ -4,8 +4,8 @@ public class Persona implements Serializable {
 		protected String nombre,documento,nacionalidad;
 		protected int edad;
 		protected boolean enfermo;
-		
-		
+		protected Maleta[] maletas;
+
 		public boolean isEnfermo() {
 			return enfermo;
 		}
@@ -21,14 +21,19 @@ public class Persona implements Serializable {
 		public int getEdad() {
 			return edad;
 		}
+		public Maleta[] getMaletas() {
+			return maletas;
+		}
 		
-		public Persona(String nombre, String documento, String nacionalidad, int edad, boolean enfermo) {
-			super();
+		public Persona(String nombre, String documento, String nacionalidad, int edad, boolean enfermo,	Maleta[] maletas) {
 			this.nombre = nombre;
 			this.documento = documento;
 			this.nacionalidad = nacionalidad;
 			this.edad = edad;
 			this.enfermo = enfermo;
+			this.maletas = maletas;
 		}
+		
+		
 		
 }
