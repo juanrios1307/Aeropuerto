de8f5077e69344ff8b34148cb37b49dc39bef5b8
juan-rios <juanesrios13@gmail.com>
2020-03-09 09:16:56 -0500

    d

--- src/Aeropuerto/Coronavirus.java
+++ src/Aeropuerto/Coronavirus.java
@@ -7,22 +7,24 @@ public class Coronavirus {
 	String direccion;
 	Vuelo Vuelo;
 	Tiquete[] personas=Vuelo.getTiquetes();
-  public boolean buscarenfermo() {
+	
+	public boolean buscarenfermo() {
 		for (int i=0;i<personas.length;i++) {
-		 if( personas[i].getPersona().isEnfermo()) {
-			 return true; }}
+			if( personas[i].getPersona().isEnfermo()) {
+				return true; 
+			}
+		}
 		return false;
-			  }
-public String getDireccion() {
-	return direccion;
-}
-public void setDireccion(String direccion) {
-	this.direccion = direccion;
-}
-		
-		
-		
-		
 	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+    
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+		
+}
 
 
