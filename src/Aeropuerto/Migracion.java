a3e233abb19c0dfc765bd7a92bbbe83fc463ab62
Decol <Decol@DESKTOP-3S4OU78>
2020-03-11 21:34:43 -0500

    final migracion

--- src/Aeropuerto/Migracion.java
+++ src/Aeropuerto/Migracion.java
@@ -32,7 +32,7 @@ public class Migracion {
 		int cont=0;
 		if (vuelo!=null) {
 			for(int i=1;i<=vuelo.getTiquetes().length;i++) {
-				 try {
+				try {
 					BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\ipool.txt"));
 					String documento ;
 					while ((documento = br.readLine()) != null) {
@@ -43,29 +43,21 @@ public class Migracion {
 								FileOutputStream fileOut = new FileOutputStream(dir);
 								ObjectOutputStream out = new ObjectOutputStream(fileOut);
 								out.writeObject(vuelo.getTiquetes()[i].getPersona());
-								if(!out.toString().isEmpty()) {
-									cont++;
-								}
-								out.close();
-						
-								fileOut.close();
 							 	Policia policia = new Policia();
 							 	policia.arrestar(dir);
-							 } catch (IOException e) {
-								 System.out.println(e.getMessage());
-							 }
+							 	return true;
+							} catch (IOException e) {
+								System.out.println(e.getMessage());
+							}
 						}
 					}
-				 } 
-				 catch (IOException e) {
+				} 
+				catch (IOException e) {
 					e.printStackTrace();
-				 }
+				}
 			}
 		}
-		if(cont>0) {
-			return true;
-		}else 
-			return false;
+		return false;
 	}
 	
 	@SuppressWarnings("unlikely-arg-type")
@@ -78,79 +70,63 @@ public class Migracion {
 					
 					for(int k=1;k<=vuelo.getTiquetes()[i].getPersona().getMaletas()[j].getMercancias().length;k++) {
 					
-						 try {
-								BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\mercancia.txt"));
-								String lote;
-								while ((lote = br.readLine()) != null) {
-									if (lote.equals(vuelo.getTiquetes()[i].getPersona().getMaletas()[j].getMercancias()[k])) {
-										try {
-											String dir = "C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\maleta.ser";
-											BufferedWriter bw = new BufferedWriter(new FileWriter(dir, false));
-											FileOutputStream fileOut = new FileOutputStream(dir);
-											ObjectOutputStream out = new ObjectOutputStream(fileOut);
-											out.writeObject(vuelo.getTiquetes()[i].getPersona().getMaletas()[j].getMercancias()[k]);
-											if(!out.toString().isEmpty()) {
-												cont++;
-											}
-											out.close();
-											fileOut.close();
-										 	Policia policia = new Policia();
-										 	policia.mercanciailegal(dir);
-										} catch (IOException e) {
-											System.out.println(e.getMessage());
-										}
+						try {
+							BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\mercancia.txt"));
+							String lote;
+							while ((lote = br.readLine()) != null) {
+								if (lote.equals(vuelo.getTiquetes()[i].getPersona().getMaletas()[j].getMercancias()[k])) {
+									try {
+										String dir = "C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\maleta.ser";
+										BufferedWriter bw = new BufferedWriter(new FileWriter(dir, false));
+										FileOutputStream fileOut = new FileOutputStream(dir);
+										ObjectOutputStream out = new ObjectOutputStream(fileOut);
+										Policia policia = new Policia();
+										policia.mercanciailegal(dir);
+										return true;
+									} catch (IOException e) {
+										System.out.println(e.getMessage());
 									}
 								}
-						 } 
-							 catch (IOException e) {
-								e.printStackTrace();
-							 }
+							}
+						} 
+						catch (IOException e) {
+							e.printStackTrace();
 						}
 					}
 				}
 			}
-		if(cont>0) {
-			return true;
-		}else
-			return false;
-		}	
+		}
+		return false;
+	}	
 	
 	public boolean revisarMercancia() {
 		int cont=0;
 		for(int i=1;i<=vuelo.getMercancia().length;i++) {
 		
-			 try {
-					BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\mercancia.txt"));
-					String lote;
-					while ((lote = br.readLine()) != null) {
-						if (lote.equals(vuelo.getMercancia()[i])) {
-							try {
-								String dir = "C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\mercancia.ser";
-								BufferedWriter bw = new BufferedWriter(new FileWriter(dir, false));
-								FileOutputStream fileOut = new FileOutputStream(dir);
-								ObjectOutputStream out = new ObjectOutputStream(fileOut);
-								out.writeObject(vuelo.getMercancia()[i]);
-								if(!out.toString().isEmpty()) {
-									cont++;
+			try {
+				BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\mercancia.txt"));
+				String lote;
+				while ((lote = br.readLine()) != null) {
+					if (lote.equals(vuelo.getMercancia()[i])) {
+						try {
+							String dir = "C:\\Users\\jergf\\Downloads\\Proyectos lenguajes made in windows\\EstructurasDeDatos\\Aeropuerto\\archivosTexto\\mercancia.ser";
+							BufferedWriter bw = new BufferedWriter(new FileWriter(dir, false));
+							FileOutputStream fileOut = new FileOutputStream(dir);
+							ObjectOutputStream out = new ObjectOutputStream(fileOut);
+							Policia policia = new Policia();
+							policia.mercanciailegal(dir);
+							return true;
+								} catch (IOException e) {
+									System.out.println(e.getMessage());
 								}
-								out.close();
-								fileOut.close();
-							 	Policia policia = new Policia();
-							 	return policia.mercanciailegal(dir);
-							} catch (IOException e) {
-								System.out.println(e.getMessage());
-							}
-						}
 					}
-			 } 
-				 catch (IOException e) {
-					e.printStackTrace();
-				 }
+				}
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
 			}
-		if(cont>0) {
-			return true;
-		}else
-			return false;
+		}
+		return false;
 	}
 
 }
